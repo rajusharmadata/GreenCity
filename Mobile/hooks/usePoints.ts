065a3d39650711ccf44c769a4158ba1c72811157
rajusharmadata <rajusharmdata123@gmail.com>
2026-07-13 15:31:06 +0530

    Remove old Mobile folder and clean up backend files before merge

--- Mobile/hooks/usePoints.ts
+++
@@ -1,7 +0,0 @@
-import { useAuthStore } from '../store/authStore';
-
-export function usePoints() {
-  const points = useAuthStore((s) => s.user?.points ?? 0);
-  const updatePoints = useAuthStore((s) => s.updatePoints);
-  return { points, updatePoints };
-}
